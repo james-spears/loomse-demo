525ded9d1756cbec05ca9177d67a4cafd2631f9f
James Spears <james.w.spears@gmail.com>
2020-10-18 12:29:05 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# LoomSE Demo Project
+
+This project intendeds to provide a starting point for users of LoomSE.
+
+
+## Install Dependencies
+
+Start by installing the project dependencies using either `npm` or `yarn`.
+
+Using `npm` type:
+
+`npm i`
+
+Using `yarn` type:
+
+`yarn install`
+
+## Build Project
+
+The `package.json` `scripts` field has been preconfigured with a build command that
+uses `webpack`. 
+
+Using `npm` type:
+
+`npm run-script build`
+
+Using `yarn` type:
+
+`yarn build`
+
+## Run LoomSE in the Browser
+
+The previous build command should have produced a directory `dist` in the project root folder. Open `dist/index.html`
+using your preferred browser and see LoomSE in action. 
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+import LoomSE from 'loomse';
+import text from './story-script.json';
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'LoomSE'], ' ');
+
+    const parent = document.querySelector('#loom-se');
+    const loomSE = new LoomSE(parent, {
+        width: 800,
+        height: 600
+    });
+    loomSE.startScript(text);
+
+    loomSE.el.addEventListener('director:sceneevent', ({ detail }) => {
+        const { group, action, payload } = detail;
+
+        if (action !== 'start') {
+            const contentContainerElement = document.querySelector('#content-container');
+            contentContainerElement.remove();
+            return;
+        }
+
+        if(group === 'skipTo') {
+            loomSE.skipTo(payload.nextScene);
+        }
+
+        if(group === 'showContent') {
+            const contentElement = document.createElement('div');
+            contentElement.id = "content";
+            contentElement.classList.add("text-content");
+
+            const textContent = document.createTextNode(payload.text);
+            contentElement.appendChild(textContent);
+
+            const contentContainerElement = document.createElement('div');
+            contentContainerElement.id = "content-container";
+            contentContainerElement.appendChild(contentElement);
+
+            parent.appendChild(contentContainerElement);
+            console.log(payload);
+        }
+    });
+
+    return element;
+}
+
+document.body.appendChild(component());
